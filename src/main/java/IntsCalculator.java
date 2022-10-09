4b5591e550ca27fbc450457e812bdb6c5d16ecc3
ditroyspb <avasilyev.gsg@gmail.com>
2022-10-09 21:37:24 +0300

    initial commit

---
+++ src/main/java/IntsCalculator.java
@@ -0,0 +1,26 @@
+public class IntsCalculator implements Ints {
+
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        Calculator.Formula result = target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM);
+        return (int) result.result();
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        Calculator.Formula result = target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT);
+        return (int) result.result();
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        Calculator.Formula result = target.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW);
+        return (int) result.result();
+    }
+}
